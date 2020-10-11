f4e1139cefa1e994dfefe7144ddb53ec0cdbf20e
john <jinjihoon@gmail.com>
2020-10-11 12:55:54 +0900

    [jin] 참여 페이지에서 카테고리 별로 보이게 수정

--- src/components/Participate.jsx
+++ src/components/Participate.jsx
@@ -1,66 +1,123 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import List from "./List";
 import styles from "./Participate.module.css";
 import Mission from "./Mission";
 import { Link } from "react-router-dom";
 
-class Participate extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      missions: [],
-      length: null,
-    };
-  }
+const Participate = () => {
+  const [fashions, SetFashions] = useState([])
+  const [its, SetIts] = useState([])
+  const [foods, SetFoods] = useState([])
 
-  getList = async () => {
-    const missions = await axios.get("http://localhost:5000/api/mission/list");
-    const dataArr = missions.data;
-    // missions.map((r) => console.log(r.goal));
-    // console.log(missions.data);
-    console.log(dataArr);
-    this.setState({ missions: dataArr, length: dataArr.length });
-    console.log(dataArr.length);
-  };
-  componentDidMount() {
-    this.getList();
-  }
+  const getItems = async () => {
+    const fashion = await axios.get("http://localhost:5000/api/mission/list/fashion")
+    const it = await axios.get("http://localhost:5000/api/mission/list/it")
+    const food = await axios.get("http://localhost:5000/api/mission/list/food")
 
-  render() {
-    const { length } = this.state;
+    SetFashions(fashion.data)
+    SetIts(it.data)
+    SetFoods(food.data)
     
-    return (
-      <>
-        <span className={styles.intro}>
-          <span className={styles.intro2}>{length}</span>개의 미션이 있습니다.
-        </span>
-        <section className={styles.container}>
-          <div className={styles.missions}>
-            
-            {this.state.missions &&
-              this.state.missions.map((mission) => (
-                // <Link to ={`/mission/${Mission._id}`}>
-                 <Link to ={`/mission/${Mission._missionId}`}> 
-                <List
-                key={mission._id}
-                  id={mission._id}
-                  goal={mission.goal}
-                  title={mission.title}
-                  image={mission.image}
-                  category={mission.category}
-                  tag={mission.tag}
-                  reward={mission.reward}
-                  account={mission.account}
-                  missionId={mission.missionId}
-                />
-                </Link>
-              ))}
-          </div>
-        </section>
-      </>
-    );
   }
-}
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  return (
+    <div className="padding-top-4e">
+      <div className={styles.categoryWhite} >
+      <p className="font-bold-700 font-1H font-color-lightgray">패션</p>
+      <span className={styles.intro}>
+        <span className={styles.intro2}>{fashions.length}</span>개의 미션이 있습니다.
+      </span>
+      <section className={styles.container}>
+        <div className={styles.missions}>
+          
+          {fashions &&
+            fashions.map((fashion) => (
+               <Link to ={`/mission/${Mission._missionId}`}> 
+              <List
+              key={fashion._id}
+                id={fashion._id}
+                goal={fashion.goal}
+                title={fashion.title}
+                image={fashion.image}
+                category={fashion.category}
+                tag={fashion.tag}
+                reward={fashion.reward}
+                account={fashion.account}
+                missionId={fashion.missionId}
+              />
+              </Link>
+            ))}
+        </div>
+      </section>
+      </div>
+
+      <div className={styles.categoryGray} >
+      <p className="font-bold-700 font-1H font-color-lightgray">IT</p>
+      <span className={styles.intro}>
+        <span className={styles.intro2}>{its.length}</span>개의 미션이 있습니다.
+      </span>
+      <section className={styles.container}>
+        <div className={styles.missions}>
+          
+          {its &&
+            its.map((it) => (
+               <Link to ={`/mission/${Mission._missionId}`}> 
+              <List
+              key={it._id}
+                id={it._id}
+                goal={it.goal}
+                title={it.title}
+                image={it.image}
+                category={it.category}
+                tag={it.tag}
+                reward={it.reward}
+                account={it.account}
+                missionId={it.missionId}
+              />
+              </Link>
+            ))}
+        </div>
+      </section>
+      </div>
+
+      <div className={styles.categoryWhite} >
+      <p className="font-bold-700 font-1H font-color-lightgray">음식</p>
+      <span className={styles.intro}>
+        <span className={styles.intro2}>{foods.length}</span>개의 미션이 있습니다.
+      </span>
+      <section className={styles.container}>
+        <div className={styles.missions}>
+          
+          {foods &&
+            foods.map((food) => (
+               <Link to ={`/mission/${Mission._missionId}`}> 
+              <List
+              key={food._id}
+                id={food._id}
+                goal={food.goal}
+                title={food.title}
+                image={food.image}
+                category={food.category}
+                tag={food.tag}
+                reward={food.reward}
+                account={food.account}
+                missionId={food.missionId}
+              />
+              </Link>
+            ))}
+        </div>
+      </section>
+      </div>
+
+
+    </div>
+    
+  );
+};
 
 export default Participate;
