75050ca10e78af498c7b913b605245e4f6ca7d4b
김영일 <rladuddlf3@naver.com>
2020-10-13 15:57:16 +0900

    CSS 수정중3

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -13,27 +13,34 @@ const List = ({
   reward,
   tag,
   length,
-  missionId
+  missionId,
+  deadline
 }) => {
   
   return (
     <>
-    <Link to ={{pathname: `/mission/${missionId}`, state : {id, title, image, category, goal, reward, tag}}}>
+    <Link to ={{pathname: `/mission/${missionId}`, state : {id, deadline, title, image, category, goal, reward, tag}}}>
       <section className={styles.container}>
         <div className={styles.mission}>
           <img className={styles.img} src={image} />
           {/* <div className={styles.deadline}>{deadline}</div> */}
-          <h5 className={styles.title}>{title} </h5>
-          {/* <h5 className={styles.category}>{category}</h5> */}
-          <h3 className={styles.goal}>{goal}</h3>
-          <h3 className={styles.reward}>{reward}</h3>
-          <h3 className={styles.tag}>{tag}</h3>
+          <h5 className={styles.title}>{title} 타이틀 </h5>
+          <h5 className={styles.category}>{category} 카테고리 </h5>
+          <h3 className={styles.goal}>{goal} 달성숫자 / 목표 </h3>
+          <h3 className={styles.summary}>나는 누구인가 커피를 오늘 
+안 먹었는데 어찌하면 좋단 말인가</h3>
+          <hr className={styles.line} />
+          <h3 className={styles.deadline}>{deadline} 마감</h3>
+          <h3 className={styles.reward}>{reward}보상 </h3>
+          <h3 className={styles.tag}>{tag} 태그</h3>
+          
           {/* <h3 className={styles.account}>{account}</h3> */}
 
           {/* <p className={styles.summary}>dddddddd</p> */}
         </div>
       </section>
       </Link>
+      
     </>
   );
 };
